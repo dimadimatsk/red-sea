d2f49459b0ce88ddf2e4018ab3830852ceb814fc
Dmitriy Shilovskiy <dimareectsk@gmail.com>
2024-02-17 12:02:28 +0700

    add category

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,42 +1,52 @@
-import React from 'react';
-import { Button } from '../Button/Button';
-import { useDispatch, useSelector } from 'react-redux';
-import { changeCategory } from '../../store/categorySlice/categorySlice';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { auth } from '../../firebase';
-import { useNavigate } from 'react-router-dom';
+import React from 'react'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { auth } from '../../firebase'
+import { changeCategory } from '../../store/categorySlice/categorySlice'
+import { Button } from '../Button/Button'
 
 export const Header = () => {
-  const categories = [
-    'Вся продукция',
-    'Кулинария из рыбы и морепродуктов',
-    'Рыба холодного копчения',
-    'Рыба вяленая',
-    'Рыба горячего копчения',
-    'Рыба слабосолёная',
-    'Морепродукты мороженые',
-    'Филе рыбное замороженное',
-    'Стейки',
-    'Полуфабрикаты рыбные рубленые замороженные',
-    'Рыба морская свежемороженая',
-    'Рыба речная свежемороженая',
-    'Консервы',
-    'Сопутствующие товары',
-    'Омега-3',
-    'Икра',
-  ];
+	const categories = [
+		'Вся продукция',
+		'Кулинария из рыбы и морепродуктов',
+		'Рыба холодного копчения',
+		'Рыба вяленая',
+		'Рыба горячего копчения',
+		'Рыба слабосолёная',
+		'Морепродукты мороженые',
+		'Филе рыбное замороженное',
+		'Стейки',
+		'Полуфабрикаты рыбные рубленые замороженные',
+		'Рыба морская свежемороженая',
+		'Рыба речная свежемороженая',
+		'Консервы',
+		'Сопутствующие товары',
+		'Омега-3',
+		'Икра',
+		'Снековая продукция',
+	]
 
-  const [user, loading, error] = useAuthState(auth);
+	const [user, loading, error] = useAuthState(auth)
 
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+	const dispatch = useDispatch()
+	const navigate = useNavigate()
 
-  return (
-    <div className="grid grid-cols-4 xsm:grid-cols-2 sm:grid-cols-4 mt-4 gap-2">
-      {categories.map((category, i) => (
-        <Button key={i} category={category} onClick={() => dispatch(changeCategory(category))} />
-      ))}
-      {user && <Button category={'Добавить продукт'} onClick={() => navigate('/dashboard')} />}
-    </div>
-  );
-};
+	return (
+		<div className='grid grid-cols-4 xsm:grid-cols-2 sm:grid-cols-4 mt-4 gap-2'>
+			{categories.map((category, i) => (
+				<Button
+					key={i}
+					category={category}
+					onClick={() => dispatch(changeCategory(category))}
+				/>
+			))}
+			{user && (
+				<Button
+					category={'Добавить продукт'}
+					onClick={() => navigate('/dashboard')}
+				/>
+			)}
+		</div>
+	)
+}
